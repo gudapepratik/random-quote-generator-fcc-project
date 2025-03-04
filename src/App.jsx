8695454d49789f90660420db26779963293a4071
Pratik <pratikgudape9825@gmail.com>
2025-03-04 19:26:10 +0530

    new-project done

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,34 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import { marked } from "marked";
+import "./App.css";
 
-function App() {
-  const [quoteDetails, setQuoteDetails] = useState({})
+const defaultMarkdown = `# Markdown Previewer
+## Type Markdown and see the preview!
+- List Item 1
+- List Item 2
+\`\`\`js
+console.log("Hello, world!");
+\`\`\`
+[Google](https://google.com)
+> Blockquote
+**Bold Text**
+`;
 
-  const fetchQuotes = async () => {
-    try {
-      const quote = await fetch('https://dummyjson.com/quotes/random/1')
-      const data = await quote.json()
-      setQuoteDetails(data[0])
-      console.log(data)
-      console.log("asf")
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  useEffect(() => {
-    fetchQuotes()
-  },[])
+export default function App() {
+  const [text, setText] = useState(defaultMarkdown);
 
   return (
-    <div className='w-full h-screen bg-amber-900 flex items-center justify-center'>
-      <div id='quote-box' className='w-[50%] p-8 flex flex-col gap-2 bg-zinc-100 min-h-40'>
-          <div id='text' className='w-full text-3xl font-bold font-serif'>
-            {quoteDetails?.quote || ""}
-          </div>
-
-          <div id='author' className='w-full text-right text-lg font-extralight'>
-            - {quoteDetails?.author || ""}
-          </div>
-
-          <div className='w-full flex justify-between mt-2'>
-            <div className='flex gap-4 items-center'>
-              <a id='tweet-quote' href={`https://twitter.com/intent/tweet?related=freecodecamp&${quoteDetails?.quote}&hashtags=${"quotes"}`} target='_top' className='bg-zinc-700 text-white p-2'>Tweet</a>
-              <a href="https://linkedin.com" target='_blank' className='bg-zinc-700 text-white p-2'>LinkedIn</a>
-            </div>
-            <button id='new-quote' className='bg-zinc-700 text-white p-2 cursor-pointer' onClick={fetchQuotes}>New quote</button>
-          </div>
-      </div>
+    <div className="container">
+      <h1>Markdown Previewer</h1>
+      <textarea
+        id="editor"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={{ __html: marked(text) }}
+      />
     </div>
-  )
+  );
 }
-
-export default App
