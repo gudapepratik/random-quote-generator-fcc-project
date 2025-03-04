9c8f4cbdc4eeb7202528105d54483540ad991f6c
Pratik <pratikgudape9825@gmail.com>
2025-03-04 19:04:11 +0530

    project done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quoteDetails, setQuoteDetails] = useState({})
+
+  const fetchQuotes = async () => {
+    try {
+      const quote = await fetch('https://dummyjson.com/quotes/random/1')
+      const data = await quote.json()
+      setQuoteDetails(data[0])
+      console.log(data)
+      console.log("asf")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    fetchQuotes()
+  },[])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='w-full h-screen bg-amber-900 flex items-center justify-center'>
+      <div id='quote-box' className='w-[50%] p-8 flex flex-col gap-2 bg-zinc-100 min-h-40'>
+          <div id='text' className='w-full text-3xl font-bold font-serif'>
+            {quoteDetails?.quote || ""}
+          </div>
+
+          <div id='author' className='w-full text-right text-lg font-extralight'>
+            - {quoteDetails?.author || ""}
+          </div>
+
+          <div className='w-full flex justify-between mt-2'>
+            <div className='flex gap-4 items-center'>
+              <a id='tweet-quote' href={`https://twitter.com/intent/tweet?related=freecodecamp&${quoteDetails?.quote}&hashtags=${"quotes"}`} target='_top' className='bg-zinc-700 text-white p-2'>Tweet</a>
+              <a href="https://linkedin.com" target='_blank' className='bg-zinc-700 text-white p-2'>LinkedIn</a>
+            </div>
+            <button id='new-quote' className='bg-zinc-700 text-white p-2 cursor-pointer' onClick={fetchQuotes}>New quote</button>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
